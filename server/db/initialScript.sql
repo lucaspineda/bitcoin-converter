eea71638a70b6c2c495186bb597539f290045156
Lucas Pineda <lucas.pineda@hotmail.com>
2024-07-28 22:04:33 -0300

    update initial script

--- server/db/initialScript.sql
+++ server/db/initialScript.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS currency (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    code VARCHAR(3) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS bitcoin_price_index (
     id INTEGER PRIMARY KEY,
     code VARCHAR(3),
@@ -10,11 +5,3 @@ CREATE TABLE IF NOT EXISTS bitcoin_price_index (
     rate_float FLOAT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO currency (id, code)
-VALUES (1, 'USD'), (2, 'GBP'), (3, 'EUR');
-
-
-
-INSERT INTO bitcoin_price_index (currency_id, symbol, rate_float)
-VALUES (1, '$', 54.2);
